261626a2e84df5ed9ce8d46e4f2bd620a87836b5
SanduniSrikantha <CB010303@students.apiit.lk>
2023-06-12 08:59:16 +0530

    login commit

--- greenthumb/lib/ProfileScreen.dart
+++ greenthumb/lib/ProfileScreen.dart
@@ -1,15 +1,82 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Future _pickImage() async {
+    final pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _imageFile = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile Screen'),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Profile',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Center(
-        child: Text('Welcome to the Profile Screen'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: 25.0), //16
+                child: Stack(
+                  alignment: Alignment.centerRight, // Align to the center-right
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundImage: _imageFile != null
+                          ? FileImage(_imageFile!)
+                          : AssetImage('assets/images/user_profile.png') as ImageProvider<Object>?,
+                    ),
+                    GestureDetector(
+                      onTap: _pickImage,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 16.0), // Add margin to the right
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProfileScreen(),
+  ));
 }
